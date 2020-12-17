858655f8fb1376220f763f12226fbc28b2b76fba
Cristian <cristianmihai193@gmail.com>
2020-12-17 16:46:15 +0100

    Metà bonus1

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
+// BONUS: Creare una select con tutti i generi dei dischi. In base a
+// cosa scegliamo nella select, vedremo i corrispondenti cd.
+
+// "poster": "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+// "title": "New Jersey",
+// "author": "Bon Jovi",
+// "genre": "Rock",
+// "year": "1988"
 var app = new Vue(
   {
     el: "#root",
     data: {
       albums: [],
-
+      genre: []
     },
     methods: {
-
+      changeGenre: function() {
+        console.log("ciao");
+      }
     },
     mounted: function() {
       let self = this;
@@ -14,8 +24,16 @@ var app = new Vue(
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then(function(result){
         self.albums = result.data.response;
+        for (var i = 0; i < self.albums.length; i++) {
+          const gen = result.data.response[i].genre
+          if (self.genre.includes(gen) == false) {
+            self.genre.push(gen)
+          }
 
+
+        }
       })
+
     }
   }
 );
